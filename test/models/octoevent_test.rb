3dd087fa4705ff8b22f37dec7386a4f5a1c7e3ed
Danielle Emygdio <emygdio.d@gmail.com>
2019-04-12 15:38:00 -0300

    Add test for model

--- test/models/octoevent_test.rb
+++ test/models/octoevent_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class OctoeventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save an octoevent without all the fields filled" do
+      octoevent = Octoevent.new
+      assert !octoevent.save
+  end
 end
